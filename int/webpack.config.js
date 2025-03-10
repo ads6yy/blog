6b2f0a7d767dc2e8a4e1cd4a3813017de98fabde
Adsy <asimon@ipsosenso.com>
2025-03-10 23:55:00 +0100

    Setup intégration Webpack + Tailwind 4 - extraction css pour futur thème Drupal

--- int/webpack.config.js
+++ int/webpack.config.js
@@ -1,13 +1,16 @@
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const JsonMinimizerPlugin = require("json-minimizer-webpack-plugin")
+
 const path = require('path')
 
 module.exports = {
-	mode: 'development',
+	mode: "production",
 	entry: './src/scripts/index.js',
 	output: {
 		path: path.join(__dirname, "build"),
-		filename: "main.js",
+		filename: "js/[name].js",
 	},
 	devServer: {
 		static: {
@@ -19,21 +22,39 @@ module.exports = {
 		compress: true,
 		historyApiFallback: true,
 	},
+	optimization: {
+		splitChunks: {
+			chunks: "all",
+			name: false,
+		},
+		minimize: true,
+		minimizer: [new JsonMinimizerPlugin()],
+	},
 	plugins: [
-		// new HtmlWebpackPlugin({
-		// 	template: path.resolve(__dirname, "src/pages/index.html"),
-		// 	filename: "index.html",
-		// }),
-		// new MiniCssExtractPlugin({
-		// 	filename: "[name].css"
-		// }),
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, "src/pages/index.html"),
+			filename: "index.html",
+		}),
+		new MiniCssExtractPlugin({
+			filename: "css/[name].css",
+			chunkFilename: "css/[name].chunk.js",
+		}),
 	],
 	module: {
 		rules: [
 			{
-				test: /\.s?css$/i,
-				include: path.resolve(__dirname, 'src'),
-				use: ['style-loader', 'css-loader', 'postcss-loader'],
+				test: /\.html$/i,
+				loader: "html-loader",
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: "babel-loader",
+			},
+			{
+				test: /\.s?css/i,
+				use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
 			},
 		],
 	},
