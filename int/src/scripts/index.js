ec8dadc3b4671c5bea7b505f4cb12fa852860f23
Adsy <asimon@ipsosenso.com>
2025-04-03 08:23:16 +0200

    Intégration - swiper projets

--- int/src/scripts/index.js
+++ int/src/scripts/index.js
@@ -1,1 +1,19 @@
 import '../styles/index.scss';
+
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('.swiper-projets', {
+	// Optional parameters
+	slidesPerView: 1.5,
+	spaceBetween: 40,
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-projets-navigation.next',
+		prevEl: '.swiper-projets-navigation.prev',
+	},
+});
