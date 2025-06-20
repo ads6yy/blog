93d9c5e5ac33d78ef29063a684929b3d42f54a04
Adsy <asimon@ipsosenso.com>
2025-06-21 01:05:48 +0200

    Intégration - view / content type / integration articles + projets + homepage

--- web/themes/perso/js/main.js
+++ web/themes/perso/js/main.js
@@ -3,5 +3,19 @@ import 'bootstrap'
 (function ($, Drupal) {
   Drupal.behaviors.main = {
 
+	  attach(context, settings) {
+		  const swiper = new Swiper('.swiper-projets', {
+			  // Optional parameters
+			  slidesPerView: 1.5,
+			  spaceBetween: 40,
+
+			  // Navigation arrows
+			  navigation: {
+				  nextEl: '.swiper-projets-navigation.next',
+				  prevEl: '.swiper-projets-navigation.prev',
+			  },
+		  });
+	  }
+
   };
 })(jQuery, Drupal);
