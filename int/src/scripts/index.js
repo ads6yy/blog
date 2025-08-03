8fd0e862fc3d0c01a8712fc729aa77184705c8e7
Adsy <asimon@ipsosenso.com>
2025-08-03 18:19:03 +0200

    Intégration - mobile + homogénéisation titre + swiper projets

--- int/src/scripts/index.js
+++ int/src/scripts/index.js
@@ -10,10 +10,23 @@ const swiper = new Swiper('.swiper-projets', {
 	// Optional parameters
 	slidesPerView: 1.5,
 	spaceBetween: 40,
-
 	// Navigation arrows
 	navigation: {
 		nextEl: '.swiper-projets-navigation.next',
 		prevEl: '.swiper-projets-navigation.prev',
 	},
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 20
+		},
+		640: {
+			slidesPerView: 1.5,
+			spaceBetween: 40
+		},
+		1280: {
+			slidesPerView: 2.5,
+			spaceBetween: 40
+		}
+	}
 });
--- web/themes/perso/js/main.js
+++ web/themes/perso/js/main.js
@@ -8,12 +8,25 @@ import 'bootstrap'
 			  // Optional parameters
 			  slidesPerView: 1.5,
 			  spaceBetween: 40,
-
 			  // Navigation arrows
 			  navigation: {
 				  nextEl: '.swiper-projets-navigation.next',
 				  prevEl: '.swiper-projets-navigation.prev',
 			  },
+			  breakpoints: {
+				  320: {
+					  slidesPerView: 1,
+					  spaceBetween: 20
+				  },
+				  640: {
+					  slidesPerView: 1.5,
+					  spaceBetween: 40
+				  },
+				  1280: {
+					  slidesPerView: 2.5,
+					  spaceBetween: 40
+				  }
+			  }
 		  });
 	  }
 
