cd0a228b506feeee19dbe9b30688f2100804c9fc
Adrien SIMON <adsysama@gmail.com>
2025-12-06 08:18:00 +0000

    Blog V3

--- web/themes/perso/js/main.js
+++ web/themes/perso/js/main.js
@@ -3,5 +3,32 @@ import 'bootstrap'
 (function ($, Drupal) {
   Drupal.behaviors.main = {
 
+	  attach(context, settings) {
+		  const swiper = new Swiper('.swiper-projets', {
+			  // Optional parameters
+			  slidesPerView: 1.5,
+			  spaceBetween: 40,
+			  // Navigation arrows
+			  navigation: {
+				  nextEl: '.swiper-projets-navigation.next',
+				  prevEl: '.swiper-projets-navigation.prev',
+			  },
+			  breakpoints: {
+				  320: {
+					  slidesPerView: 1,
+					  spaceBetween: 20
+				  },
+				  640: {
+					  slidesPerView: 1.5,
+					  spaceBetween: 40
+				  },
+				  1280: {
+					  slidesPerView: 2.5,
+					  spaceBetween: 40
+				  }
+			  }
+		  });
+	  }
+
   };
 })(jQuery, Drupal);
